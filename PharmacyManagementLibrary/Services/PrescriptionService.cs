f5fee9986b3e0f29701f87c31db542e828f49984
Sakthi (Sandy) Santhosh <166415082+ssanthosh010303@users.noreply.github.com>
2024-04-22 08:16:10 +0000

    create: prescription

---
+++ PharmacyManagementLibrary/Services/PrescriptionService.cs
@@ -0,0 +1,45 @@
+/*
+ * Author: Sakthi Santhosh
+ * Created on: 22/04/2024
+ */
+namespace Assessment1.PharmacyManagementLibrary;
+
+public interface IPrescriptionService : IManagementBaseService<Prescription>
+{
+    Prescription Add(Patient patient, Doctor doctor, List<Drug> prescribedDrugs);
+}
+
+public class PrescriptionService : IPrescriptionService
+{
+    private readonly IPrescriptionRepository _prescriptionRepository;
+
+    public PrescriptionService(IPrescriptionRepository prescriptionRepository)
+    {
+        _prescriptionRepository = prescriptionRepository;
+    }
+
+    public Prescription Add(Patient patient, Doctor doctor, List<Drug> prescribedDrugs)
+    {
+        return _prescriptionRepository.Add(patient, doctor, prescribedDrugs);
+    }
+
+    public List<Prescription> GetAll()
+    {
+        return _prescriptionRepository.GetAll();
+    }
+
+    public Prescription? GetById(int id)
+    {
+        return _prescriptionRepository.GetById(id);
+    }
+
+    public bool Update(Prescription prescription)
+    {
+        return _prescriptionRepository.Update(prescription);
+    }
+
+    public bool Delete(Prescription prescription)
+    {
+        return _prescriptionRepository.Delete(prescription);
+    }
+}
