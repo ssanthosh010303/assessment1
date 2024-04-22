d8b10aef5eabca357b831454f0922691e6632f45
Sakthi (Sandy) Santhosh <166415082+ssanthosh010303@users.noreply.github.com>
2024-04-22 08:18:44 +0000

    create: base-repository

---
+++ PharmacyManagementLibrary/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+/*
+ * Author: Sakthi Santhosh
+ * Created on: 22/04/2024
+ *
+ * Interface Definitions for Business Logic Layer
+ */
+namespace Assessment1.PharmacyManagementLibrary;
+
+public interface IBaseRepository<T> where T : class
+{
+    List<T> GetAll();
+
+    bool Update(T entity);
+    bool Delete(T entity);
+
+    T this[int index] { get; }
+}
+
+public interface IMedicalBaseRepository<T> : IBaseRepository<T> where T : class
+{
+    T? GetByName(string name);
+}
+
+public interface IManagementBaseRepository<T> : IBaseRepository<T> where T : class
+{
+    T? GetById(int id);
+}
