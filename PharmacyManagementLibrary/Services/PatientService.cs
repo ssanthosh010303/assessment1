721fd03a7a366d784bca0d0c14650742d1c0957c
Sakthi (Sandy) Santhosh <166415082+ssanthosh010303@users.noreply.github.com>
2024-04-22 08:15:44 +0000

    create: patient

---
+++ PharmacyManagementLibrary/Services/PatientService.cs
@@ -0,0 +1,45 @@
+/*
+ * Author: Sakthi Santhosh
+ * Created on: 22/04/2024
+ */
+namespace Assessment1.PharmacyManagementLibrary;
+
+public interface IPatientService : IMedicalBaseService<Patient>
+{
+    Patient Add(string name, string contact, Doctor referredDoctor);
+}
+
+public class PatientService : IPatientService
+{
+    private readonly IPatientRepository _patientRepository;
+
+    public PatientService(IPatientRepository patientRepository)
+    {
+        _patientRepository = patientRepository;
+    }
+
+    public Patient Add(string name, string contact, Doctor referredDoctor)
+    {
+        return _patientRepository.Add(name, contact, referredDoctor);
+    }
+
+    public List<Patient> GetAll()
+    {
+        return _patientRepository.GetAll();
+    }
+
+    public Patient? GetByName(string name)
+    {
+        return _patientRepository.GetByName(name);
+    }
+
+    public bool Update(Patient patient)
+    {
+        return _patientRepository.Update(patient);
+    }
+
+    public bool Delete(Patient patient)
+    {
+        return _patientRepository.Delete(patient);
+    }
+}
